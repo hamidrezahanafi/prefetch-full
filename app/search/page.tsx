fc2c316a0a60a9682e4559631726416580914e8b
Hamidreza Hanafi <hamidreza.hanafi@faire.com>
2025-11-21 19:56:04 -0500

    make page dynamic

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,9 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const SearchPage = async () => {
     const result = await new Promise<string>((resolve) =>
-        setTimeout(() => resolve(Math.random().toString(36).substring(2)), 10000)
+        setTimeout(() => resolve(Math.random().toString(36).substring(2)), 5000)
     );
     return <div>{result}</div>;
 };
